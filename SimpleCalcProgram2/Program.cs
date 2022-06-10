26163eaea14a71a6fcc403296e622af58401f4f2
bagelTheArmy <80268894+bagelTheArmy@users.noreply.github.com>
2022-06-10 11:10:14 -0400

    Add project files.

---
+++ SimpleCalcProgram2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SimpleCalcProgram2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.Write("enter a number with two decimal places -1 to exit:");
+                string strNumber = Console.ReadLine();
+                if(!(strNumber=="-1"))
+                {
+                    try
+                    {
+                        double number = double.Parse(strNumber);
+                        double finalNum = number + 2;
+                        finalNum = finalNum + 10;
+                        finalNum = finalNum + (number * .20);
+                        Console.WriteLine(finalNum);
+                    }catch(Exception e)
+                    {
+                        Console.WriteLine("not a double var");
+                    }
+                }
+                else
+                {
+                    exit = true;
+                }
+
+
+            }
+        }
+    }
+}
